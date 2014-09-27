8507b9a437df2479b3e5d82d92bad83a8babee04
Kevin <kevin_dms@hotmail.com>
2014-09-26 18:35:56 -0700

    Decifra incompleto 2
    Adicionado partes do armazena palavras e separa elas do Decifra

--- Decifra.java
+++ Decifra.java
@@ -4,16 +4,37 @@ import java.util.Scanner;
 public class Decifra {
 	public static void main(String args[]){
 		int i = 0;
-		String salvaPalavras[] = new String[2000];
-		String salvaTokens[] = new String[4000];
+		String guardaPalavras[] = new String[2000];
+		String guardaTokens[] = new String[4000];
 		
 		try{
 				File arquivoaDecifrar = new File(args[0]);
 				Scanner s = new Scanner(arquivoaDecifrar);
 			
 				while(s.hasNextLine()){
-				salvaPalavras[i] = s.nextLine();
-				i++;
+				
+					guardaPalavras[i] = s.nextLine();
+					i++;
+				
+				}
+				
+				int j = 0;
+				String armazena[] = new String[300];
+				
+				while(guardaPalavras[j]!= null){
+					
+					int a = 0;
+					armazena = guardaPalavras[j].split("\\s");
+					
+					while(guardaPalavras[a]!= null){
+						a++;
+					}
+					
+					int b;
+					for(b = 0; b < armazena.length; b++){
+						
+					
+					}
 				}
 			}
 		}
