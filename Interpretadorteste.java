3f4eab4a3e03e38d6d333bb64b11c430a5db71f1
Leduan <ledu07@hotmail.com>
2014-10-17 01:39:30 -0300

    Interpretador
    testando o principal para pegar somente linhas que estarao preenchidas

---
+++ Interpretadorteste.java
@@ -0,0 +1,23 @@
+class Interpretador{	
+
+
+	public void interpreta(String linhas[]){
+		int i = 0;
+	
+		while(linhas[i] != null){
+			System.out.println("Ola");		//testando ,ele vai pegar somente linhas que nao estiverem em branco
+			i++;
+		}
+
+	}
+	
+
+
+
+
+
+
+
+
+
+}
