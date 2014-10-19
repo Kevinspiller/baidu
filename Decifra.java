cfe24d41091a91c650da5a43471399786f2baba7
Kevinspiller <kevin_dms@hotmail.com>
2014-10-19 02:53:00 -0200

    Classe Decifra
    Foi mudado uma boa parte da logica para a classe decifra

--- Decifra.java
+++ Decifra.java
@@ -1,38 +1,47 @@
 import java.util.Scanner;
-import java.io.*;
 
-class Baidu {
-    public static void main(String args[]) throws Exception {
-        File f;
-        Scanner s;
-        Interpretador b;
-        String linhas[] = new String[2000]; // arquivo pode ter, no máximo, 2000 linhas.
-        
-        try {
-            // Referencia o arquivo. args[0] conterá os dados passados pela linha de comando.
-            f = new File(args[0]);
-            // Mandamos o Scanner ler a partir do arquivo.
-            s = new Scanner(f);
-            // Instanciamos o interpretador.
-            b = new Interpretador();
-            
-            // Lemos todas as linhas do arquivo para dentro do
-            // vetor "linhas".
-            int i = 0;
-            while(s.hasNext()) {
-                linhas[i] = s.nextLine();
-                i++;
-            }
-            
-            // Inicializamos o interpretador com o vetor de linhas. A partir
-            // desse ponto, o objeto "b" irá interpretar o código lido do arquivo.
-            b.interpreta(linhas);
-            
-        } catch (IOException e) {
-            System.out.println("Nao consegui ler o arquivo: " + (args.length > 0 ? args[0] : "(desconhecido)"));
-            System.out.println("Uso:");
-            System.out.println("    java Baidu /caminho/para/arquivo.baidu");
-        }
-        
-    }
-}
+class Decifra {
+	
+			
+		public FileReader arquivoR;//objetos para leitura
+		public BufferedReader buffer;
+		public int read=0, countLine=0, i=0;
+		
+		Variavel objVariavel = new Variavel();//objeto das variaveis
+		String [] code = new String[1000];//vetor de strings que armazena o codigo a ser tratado
+		
+		
+
+		public Decifra(String path){//construtor
+
+			try{	//tratamento de erro sobre a read do arquivo
+				arquivoR = new FileReader(path); //abre o arquivo
+				buffer = new BufferedReader(arquivoR);//metodo mais rapido
+				
+				while(buffer.ready()){
+					code[read]=buffer.readLine();//code recebe o que esta na buffer a cada linha, assim cada posicao do vetor de string tem uma linha
+					
+					code[read]=code[read].replaceAll("\t","");
+					if(code[read].startsWith("IMPRIMA('")){
+						code[read]=code[read].replaceAll(" ","|"); //retirando os espacos das strings e susbstitui por |, caso for uma impressao
+				
+					} else {
+						code[read]=code[read].replaceAll(" ",""); // retira os espacos das strings
+					}
+
+					read++;//incremento do vetor
+					countLine++;//contador de linhas
+				}
+				buffer.close();//fecha o arquivo
+			}
+
+			catch (FileNotFoundException ex){
+
+			}
+			catch (IOException er){
+
+			}
+
+			objVariavel.procuraVar(code,countLine);//declaracao de variaveis
+		}
+	}
