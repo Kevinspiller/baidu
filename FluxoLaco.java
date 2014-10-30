d593e4f91422d915b57c5b81b351f4fb5400c363
Leduan <ledu07@hotmail.com>
2014-10-30 18:20:23 -0200

    FluxoLaco
    Retirado [] onde dava erro de sintaxe e de ponteir pra pointer

--- FluxoLaco.java
+++ FluxoLaco.java
@@ -2,19 +2,17 @@ class FluxoLaco{
 
 
 	public int [] array;
-	public int [] pointer;
+	public int pointer;
 
 	public FluxoLaco(){
 
-		this.ponteir= -1;
+		this.pointer= -1;
 		this.array = new int[300];
 	}
 
 
-	public int pop{
-
+	public int pop(){
 			return this.array[this.pointer--];
-
 	}
 
 	public void push(int v){
