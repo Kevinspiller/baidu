501b6d96f3dd69db7482199f6bd45bf718ce0ad6
Leduan <ledu07@hotmail.com>
2014-10-30 15:30:25 -0200

    FluxoLaco
    Enviei o arquivo errado...

---
+++ FluxoLaco.java
@@ -0,0 +1,37 @@
+class FluxoLaco{
+
+
+	public int [] array;
+	public int [] pointer;
+
+	public FluxoLaco(){
+
+		this.ponteir= -1;
+		this.array = new int[300];
+	}
+
+
+	public int pop{
+
+			return this.vetor[this.pointer--];
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
